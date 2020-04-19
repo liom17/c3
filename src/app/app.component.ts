5db3cc28c199f75e9c49507992cf549c71898ab4
liom17 <1014596401@qq.com>
2020-04-19 13:33:54 +0800

    c3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { applySourceSpanToExpressionIfNeeded } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'CH04';
+  shouldShow = false;
+  score = 70;
+  color: string;
+  fontSize: string;
+
+
+
+  cities = ['杭州', '上海', '南京', '无锡']
+
+  onToggle() {
+    this.shouldShow = !this.shouldShow;
+    return false;
+  }
+  grade() {
+    return Math.floor(this.score / 10);
+  }
+  onInput(evt: Event) {
+    const inputEle = <HTMLInputElement>evt.target;
+    this.score = Number(inputEle.value);
+  }
+
+  apply(color: string, fontSize: string) {
+    this.color = color;
+    this.fontSize = fontSize;
+
+  }
 }
